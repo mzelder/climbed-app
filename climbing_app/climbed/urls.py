72efd1626fd9bcb0cdf587000193c18d24297df5
mzelder <maciej.zelder@wp.pl>
2024-10-18 18:47:34 +0200

    index page implementation, partially impl. user registration

--- climbing_app/climbed/urls.py
+++ climbing_app/climbed/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from climbed import views
 
 urlpatterns = [
-    path("", views.index, name="home")
+    path("", views.index, name="index"),
+    path("register", views.register, name="register"),
+    path("login", views.login, name="login"),
+    path("logout_view", views.logout_view, name="logout")
 ]
--- climbing_app/climbed/views.py
+++ climbing_app/climbed/views.py
@@ -1,5 +1,34 @@
-from django.shortcuts import render, HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 def index(request):
     return render(request, "climbed/index.html")
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+
+    form = UserCreationForm()
+    return render(request, "climbed/register.html", {'form': form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect("index")
+
+    return render(request, {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
