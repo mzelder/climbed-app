f5b9e207114cffa1546786ac4a7f0e4f49fbb238
mzelder <maciej.zelder@wp.pl>
2024-10-20 10:04:00 +0200

    sign in, sign out functionality

--- climbing_app/climbed/views.py
+++ climbing_app/climbed/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, HttpResponse, redirect
+from django.db import IntegrityError
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
@@ -8,24 +9,48 @@ def index(request):
     return render(request, "climbed/index.html")
 
 def login_view(request):
-    # if request.method == "POST":
-        #user = authenticate(username=username, password=password)
-        # if user:
-        #     login(request, user)
-        #     return redirect("index")
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
 
-    return render(request, "climbed/login.html")
+        if user is not None:
+            login(request, user)
+            return redirect("index")
+        else:
+            return render(request, "climbed/login.html", {
+                "message": "Invalid name and/or username",
+                "status": "error"
+            })
+    else:
+        return render(request, "climbed/login.html")
 
 def logout_view(request):
     logout(request)
-    return redirect("login")
+    return redirect("index")
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("index")
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        
+        if password != confirmation:
+            return render(request, "climbed/register.html", {
+                "message": "Passwords must match.",
+                "status": "error"
+            })
 
-    form = UserCreationForm()
-    return render(request, "climbed/register.html", {'form': form})
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "climbed/register.html", {
+                "message": "Username already taken.",
+                "status": "error"
+            })
+        login(request, user)
+        return redirect("index")
+    else:
+        return render(request, "climbed/register.html")
