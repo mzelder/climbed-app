d565415c8f11bcb9a25c3cd71e5f149e47ed905b
mzelder <maciej.zelder@wp.pl>
2024-11-29 14:23:31 +0100

    sql update

--- climbing_app/climbing_app/settings.py
+++ climbing_app/climbing_app/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = "climbing_app.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "data/db.sqlite3"
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
